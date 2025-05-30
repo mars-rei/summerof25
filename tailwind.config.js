aed81db262d4755f28111f14c46a9f2c11d22a1b
mars <imidcen@gmail.com>
2025-05-30 12:30:01 +0100

    back to the drawing board - tailwind css is finally working!!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
   content: [
     './index.html',
-    './**/*.html',
-    './**/*.js',
-  ],
+  ], 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark1: 'var(--dark1)',
+        dark2: 'var(--dark2)',
+        light1: 'var(--light1)',
+        light2: 'var(--light2)',
+        green1: 'var(--green1)',
+        green2: 'var(--green2)',
+      },
+    },
   },
-}
+  darkMode: 'class',
+};
+
