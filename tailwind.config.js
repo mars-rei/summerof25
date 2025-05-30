1b689cc4ebe9ece71f3da4180cb1c663d44a7487
mars <imidcen@gmail.com>
2025-05-30 16:36:16 +0100

    checklist,, tags,, and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 module.exports = {
   content: [
     './index.html',
+    './testing.html',
+    './css/main.css',
+    './src/input.css',
   ], 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark1: 'var(--dark1)',
+        dark2: 'var(--dark2)',
+        light1: 'var(--light1)',
+        light2: 'var(--light2)',
+        green1: 'var(--green1)',
+        green2: 'var(--green2)',
+      },
+    },
   },
   darkMode: 'class',
   plugins: [],
