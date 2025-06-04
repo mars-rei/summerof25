1651f87fdb3c1687459a60fe7c870e593ce3864e
mars <imidcen@gmail.com>
2025-06-04 11:30:29 +0100

    finished 0524 blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
 module.exports = {
   content: [
-    './index.html'
+    './index.html',
+    './0524.html'
   ], 
   theme: {
     extend: {
-      colors: {
-        dark1: 'var(--dark1)',
-        dark2: 'var(--dark2)',
-        light1: 'var(--light1)',
-        light2: 'var(--light2)',
-        green1: 'var(--green1)',
-        green2: 'var(--green2)',
-      },
     },
   },
   darkMode: 'class',
